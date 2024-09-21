e5d5481fcdcab4ec36c26f9979d781ba54440252
Chris Dillard <cdillard7514@yahoo.com>
2024-09-21 11:53:40 -0400

    completed the Edit functionality

--- scripts/edit-profile.js
+++ scripts/edit-profile.js
@@ -1,15 +1,36 @@
 const editProfileBtn = document.querySelector(".profile__button");
 const closeProfileBtn = document.querySelector(".modal__close-button");
 const modal = document.querySelector(".modal");
+const formElement = document.querySelector(".edit");
+const profileNameElement = document.querySelector(".profile__title");
+const profileJobElement = document.querySelector(".profile__subtitle");
+const editProfileName = document.querySelector(".edit__profile-name");
+const editProfileDescription = document.querySelector(
+  ".edit__profile-description"
+);
 
+// Open Edit Profile Modal
 function openEditProfileModal() {
   modal.classList.add("modal__open");
-}
 
+  editProfileName.value = profileNameElement.textContent;
+  editProfileDescription.value = profileJobElement.textContent;
+}
 editProfileBtn.addEventListener("click", openEditProfileModal);
 
-function closeEditProfileModal() {
+// Make changes to the inputs in the modal
+function handleProfileFormSubmit(e) {
+  e.preventDefault();
+
+  profileNameElement.textContent = editProfileName.value;
+  profileJobElement.textContent = editProfileDescription.value;
+
+  closeEditProfileModal();
+}
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+// Close Edit Profile Modal without saving changes
+function closeEditProfileModal(e) {
   modal.classList.remove("modal__open");
 }
-
 closeProfileBtn.addEventListener("click", closeEditProfileModal);
