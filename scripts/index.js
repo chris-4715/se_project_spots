d7b8397c7471bde5800c7d490f24f6eae82a5875
Chris Dillard <cdillard7514@yahoo.com>
2024-09-22 18:41:46 -0400

    fixing issues per feedback

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,7 @@ import { initialCards } from "./initial-cards.js";
 const template = document.querySelector("#post").content;
 const posts = document.querySelector(".posts");
 
-function renderCards(cards) {
-  for (let card of cards) {
-    const postElement = template.querySelector(".post").cloneNode(true);
-    postElement.querySelector(".post__author").textContent = `${card.author}`;
-
-    const postImage = postElement.querySelector(".post__image");
-    postImage.src = `${card.link}`;
-    postImage.alt = `${card.author}`;
-
-    posts.append(postElement);
-  }
-
-  return cards;
-}
-renderCards(initialCards);
-
+// Create card function
 function createCard(card) {
   const postElement = template.querySelector(".post").cloneNode(true);
   postElement.querySelector(".post__author").textContent = `${card.author}`;
@@ -27,5 +12,17 @@ function createCard(card) {
   postImage.src = `${card.link}`;
   postImage.alt = `${card.author}`;
 
-  return posts.append(postElement);
+  posts.append(postElement);
+
+  return card;
 }
+
+// Render existing cards function
+function renderCards(cards) {
+  for (let card of cards) {
+    createCard(card);
+  }
+
+  return cards;
+}
+renderCards(initialCards);
