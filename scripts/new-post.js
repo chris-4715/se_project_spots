0d932c81ecb969742647600df09c4aac2f90e7b5
Chris Dillard <cdillard7514@yahoo.com>
2024-10-01 06:55:25 -0400

    add new post functionality

--- scripts/new-post.js
+++ scripts/new-post.js
@@ -1,13 +1,15 @@
 const newPostBtn = document.querySelector(".profile__post-button");
 const closeNewPostModalBtn = document.querySelector(".new-post__close-button");
 const newPost = document.querySelector(".new-post");
+const newPostForm = document.querySelector(".new-post__form");
 const newPostModal = document.querySelector(".new-post-modal");
-const newPostElement = document.querySelector("[name='new-post__form']");
 const postAuthorElement = document.querySelector(".post__author");
 const postImageElement = document.querySelector(".post__image");
 const postLikeElement = document.querySelector(".post__like");
 const newPostLink = document.querySelector("[name='image-link']");
 const newPostCaption = document.querySelector("[name='caption']");
+const savePostBtn = document.querySelector(".new-post__save-button");
+const posts = document.querySelector(".posts");
 
 // Open New Post Modal
 function openNewPostModal(newPost) {
@@ -18,15 +20,21 @@ newPostBtn.addEventListener("click", () => {
 });
 
 // Make changes to the inputs in the modal
-function handleNewPostFormSubmit(e) {
-  e.preventDefault();
+function handleNewPostFormSubmit() {
+  const newPostTemplate = document.querySelector("#post").content;
+  const newPostElement = newPostTemplate.querySelector(".post").cloneNode(true);
+  newPostElement.querySelector(".post__image").src = newPostLink.value;
+  newPostElement.querySelector(".post__author").textContent =
+    newPostCaption.value;
 
-  postImageElement.textContent = newPostLink.value;
-  postAuthorElement.textContent = newPostCaption.value;
+  posts.append(newPostElement);
 
-  closeEditProfileModal();
+  closeNewPostModal();
 }
-formElement.addEventListener("submit", handleProfileFormSubmit);
+newPostForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  handleNewPostFormSubmit(e);
+});
 
 // Close Edit Profile Modal without saving changes
 function closeNewPostModal(e) {
